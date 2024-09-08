8a88c1c509968a0e5f5ec4e4be44f6f256af25c4
Alex Dolid <dolid.sasha@gmail.com>
2024-09-08 14:43:11 +0300

    [redo] TNegInt and TPosInt

---
+++ lib/TNumber/TInt/TNegInt/TNegInt.d.ts
@@ -0,0 +1,4 @@
+import type { Options } from '../../../types';
+
+export declare function TNegInt(value: unknown, options?: Options): value is number;
+export declare function isNegInt(value: number, options?: Options): boolean;
---
+++ lib/TNumber/TInt/TPosInt/TPosInt.d.ts
@@ -0,0 +1,4 @@
+import type { Options } from '../../../types';
+
+export declare function TPosInt(value: unknown, options?: Options): value is number;
+export declare function isPosInt(value: number, options?: Options): boolean;
